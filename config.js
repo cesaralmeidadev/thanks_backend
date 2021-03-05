4156085e49d4232dd9f882cf1ee638b02fddb467
Cesar Almeida <cesaralmeidareyes@gmail.com>
2021-03-05 11:56:52 +0100

    DB connected and structure modified

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
+
 dotenv.config();
+
 export default {
 	port: process.env.PORT || 3000,
+	db: {
+		postDB: process.env.POSTDB_URI,
+	},
 };
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import config from "./config.js";
+import postDB from "./store/postDB.js";
 
 const app = express();
+
 const port = config.port;
+const uri = config.db.postDB;
 
-app.use("/", (req, res) => {
-    res.send('Hello World')
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+	postDB(uri);
 });
-
-app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
